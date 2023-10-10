b2d6fabfd268aae9f1b03cdea6229fda0eeba714
Ehsan Jalilifar <ehsan.jalilifar@gmail.com>
2023-10-10 15:16:30 -0500

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Ehsan Jalilifar on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
